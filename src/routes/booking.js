b879dca17e4ca78d8cc9ec1b0bde79f2a09cd571
Raj Sahu <55652441+rajsahu-git@users.noreply.github.com>
2026-01-06 13:48:14 +0530

    bug fix

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const bookingController = require('../controllers/bookingController');
+const { bookingController, upload } = require('../controllers/bookingController');
 const router = express.Router();
 
-router.post('/', bookingController.create);
+router.post('/', upload.single('file'), bookingController.create);
 router.get('/', bookingController.getAll);
 router.get('/:id', bookingController.getById);
 router.put('/:id', bookingController.update);
@@ -10,5 +10,8 @@ router.delete('/:id', bookingController.delete);
 
 // Guest management
 router.post('/:booking_id/guests/bulk', bookingController.bulkUploadGuests);
+
+// Upload guests with room allocation
+router.post('/:booking_id/guests/upload-allocate', upload.single('file'), bookingController.uploadGuestsWithAllocation);
 
 module.exports = router;
